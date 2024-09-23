d31fd9e1e9529a0cb10b1ced4c2975080c0e0c83
chumanhtien <tiencm194182@gmail.com>
2024-09-23 10:05:22 +0700

    update bai 10: them text tu input

--- oop_assignment/bai_10/src/main.cpp
+++ oop_assignment/bai_10/src/main.cpp
@@ -1,8 +1,14 @@
 #include <iostream>
 #include "../inc/Text.h"
+using namespace std;
 
 int main() {
-    Text txt("         Hello, I'm Chu Manh Tien. I'm from Vietnam. Vietnam is the best.             ");
+    // Text txt("         Hello, I'm Chu Manh Tien. I'm from Vietnam. Vietnam is the best.             ");
+    std::string txtTest;
+    cout << "Input text: ";
+    getline(cin, txtTest);
+
+    Text txt(txtTest);
 
     std::cout << "Original Text: \"" << txt.getText() << "\"\n";
     std::cout << "Number of words: " << txt.countWords() << "\n";
